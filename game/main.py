d0da032e947b168a6a70bb5cf636514de6edebf3
Михаил <mikhail.povor.77@mail.ru>
2024-04-02 22:05:47 +0300

    2.04.2024

--- game/main.py
+++ game/main.py
@@ -8,16 +8,18 @@ def run():
     pygame.display.set_caption("Awarnes")
     # цвет заднего фона
     bg_color = (0,0,0)
-    #gun = Gun(screen)
+    gun = Gun(screen)
     
       #Добавление музыки
     # pygame.mixer.music.load("Часть_2/sound/background.ogg")
     # pygame.mixer.music.play()
     #Игровой цикл
     while True:
-        controls.events(screen)
-        controls.update(screen)
-        
+        controls.events(screen, gun, bullets)    # обрабатываем события нажатия клавиш ставим True или False
+        gun.update_gun()        # если клавиша нажата смещаем космо аппарат
+        controls.update(screen,gun,bg_color,bullets)
+        controls.update_bullets(bullets)
+
 
 run()
         
