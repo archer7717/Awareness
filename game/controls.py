d9a9f8b615a466b5185f5c888448647bc9c46bf8
Сергей Сауляк <sergejsaulak7@gmail.com>
2024-04-02 22:41:22 +0300

    aboba

---
+++ game/controls.py
@@ -0,0 +1,15 @@
+import pygame 
+
+       
+def go():
+ 
+ speed=20
+ while K_left == True:
+    print(self.rect.centerx - speed) 
+    
+
+ while k_right == True:
+        print(self.rect.bottom + speed)_
+        
+        
+    
---
+++ game/gun.py
@@ -0,0 +1,14 @@
+import pygame 
+
+class Gun():
+    def _init_(self,speed,health,damage):
+        speed.self=speed
+        health.self=health
+        damage.self=damage
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+
+ 
+              
+
